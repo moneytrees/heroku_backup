0550b5b06ed064c7450a0a7f1373db05a838a33e
Thais Cailet <thais.cailet@gmail.com>
2018-07-18 21:48:35 -0500

    Adds MONGODB env var on other missing spots

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const keys = require(__basedir + "config/keys");
+const keys = process.env.MONGODB || require(__basedir + "config/keys");
 const passport = require("passport");
 require("dotenv").config();
 
