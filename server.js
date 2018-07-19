3179abfd2b93e86cc9e3af8d65d1bc3bf55bd91c
Thais Cailet <thais.cailet@gmail.com>
2018-07-18 22:39:13 -0500

    Attempts to serve index.html one more time

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('./client/build'));
     const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve('app', 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 app.get('*.js', function (req, res, next) {
