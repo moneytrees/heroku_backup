d29bd5be1cf04546fa7e6ceb64bc1c091cebdc66
Thais Cailet <thais.cailet@gmail.com>
2018-07-18 22:53:00 -0500

    Removes proxy and serves static files

--- server.js
+++ server.js
@@ -17,10 +17,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./client/build'));
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
 }
 app.get('*.js', function (req, res, next) {
